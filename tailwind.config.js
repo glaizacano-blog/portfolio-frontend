9eeef2f223661366c8bb631cba441588e5c2c892
Glaiza Cano <glaizacn@gmail.com>
2021-04-20 22:09:35 +1000

    :lipstick: Update Tailwind CSS configuration and add utility stylesheets
    Update tailwind config and add fonts and gradients stylesheets

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,13 @@ module.exports = {
       purple: colors.purple,
       fuchsia: colors.fuchsia,
       pink: colors.pink,
-      rose: colors.rose
+      rose: colors.rose,
+      linkedin: '#0077b5',
+      github: '#191717',
+      gitlab: '#fc6d27',
+      twitter: '#1dA1f2',
+      dev: '#000000',
+      instagram: '#e4405f',
     },
     screens: {
       'xs': {'min': '0px', 'max': '575px'},
@@ -36,18 +42,62 @@ module.exports = {
       '2xl': {'min': '1536px'},
     },
     extend: {
+      screens: {
+        '3xl': '1600px',
+      },
+      fontSize: {
+        '4.5xl': '2.5rem',
+        '4.75xl': '2.75rem',
+        '5.25xl': '3.25rem',
+        '5.5xl': '3.5rem'
+      },
       height: {
         '66': '16.5rem',
         '68': '17rem',
+        '84': '21rem',
         '88': '22rem',
+        '100': '26rem',
+        '104': '28rem',
+        '106': '30rem',
+        '108': '32rem',
+        '112': '36rem',
+        '114': '38rem',
+        '116': '40rem',
+        '120': '44rem',
         '1/2-screen': '50vh',
         '2/3-screen': '66.666667vh',
         '3/4-screen': '75vh',
+        'nav': '5rem'
       },
+      maxHeight: {
+        '100': '26rem',
+        '114': '38rem',
+      },
+      minHeight: {
+        '96': '24rem',
+        '98': '25rem',
+        '114': '38rem',
+      },
+      maxWidth: {
+        '1/4': '25%',
+        '1/3': '33.333333%',
+        '1/2': '50%',
+        '5/6': '83.333333%;'
+      },
+      minWidth: {
+        '1/4': '25%',
+        '2/5': '40%',
+      }
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundClip: ['hover', 'focus'],
+      backgroundImage: ['hover', 'focus'],
+      borderColor: ['hover', 'focus'],
+      borderWidth: ['hover', 'focus'],
+      padding: ['hover', 'focus']
+    },
   },
   plugins: [],
 }
