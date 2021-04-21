c964b29b89c9f2f8f90ad156d54ee0b0882a0006
Glaiza Cano <glaizacn@gmail.com>
2021-04-21 20:09:44 +1000

    :art: Refactor app and add stylesheets
    Refactor App.js and index.js, and add stylesheets for App.js and index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
-import App from './components/App';
+import App from './containers/App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = document.getElementById('root');
+
+ReactDOM.render(<App />, root);
