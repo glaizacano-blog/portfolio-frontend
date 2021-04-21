08add452f9a88d5e3ea00219143d0ddd3dd5d8b0
Glaiza Cano <glaizacn@gmail.com>
2021-04-21 19:28:28 +1000

    :sparkles: Add UI components
    Add Navbar component, Header component, Footer component, and ScrollToTop component. Add stylesheet for navigation menu links.

---
+++ src/components/UI/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="relative bottom-0 w-full m-0">
+      <footer className="flex justify-center items-center bg-black h-14">
+        <p className="font-medium text-white text-center ">
+          Copyright &copy; 2021 Glaiza Cano
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
